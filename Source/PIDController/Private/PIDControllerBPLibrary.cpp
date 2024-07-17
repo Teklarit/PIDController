bb71a4719215ada89cca1c4c8802910f45550dc7
Teklarit <Teklarit@yandex.ru>
2024-07-18 00:42:16 +0200

    Better name.

--- Source/PIDController/Private/PIDControllerBPLibrary.cpp
+++ Source/PIDController/Private/PIDControllerBPLibrary.cpp
@@ -3,22 +3,22 @@
 #include "PIDControllerBPLibrary.h"
 #include "PIDCore.h"
 
-void UPIDControllerBPLibrary::InitPID(FPIDCore& PIDRef, const float InKp, const float InKi, const float InKd)
+void UPIDControllerBPLibrary::InitPID(FPIDCore& PIDCoreRef, const float InKp, const float InKi, const float InKd)
 {
-	PIDRef.Init(InKp, InKi, InKd);
+	PIDCoreRef.Init(InKp, InKi, InKd);
 }
 
-float UPIDControllerBPLibrary::TickPID(FPIDCore& PIDRef, const float Difference, const float DeltaTime)
+float UPIDControllerBPLibrary::TickPID(FPIDCore& PIDCoreRef, const float Difference, const float DeltaTime)
 {
-	return PIDRef.Tick(Difference, DeltaTime);
+	return PIDCoreRef.Tick(Difference, DeltaTime);
 }
 
-float UPIDControllerBPLibrary::GetSignalPID(const FPIDCore& PIDRef)
+float UPIDControllerBPLibrary::GetSignalPID(const FPIDCore& PIDCoreRef)
 {
-	return  PIDRef.GetSignalValue();
+	return  PIDCoreRef.GetSignalValue();
 }
 
-void UPIDControllerBPLibrary::ResetPID(FPIDCore& PIDRef)
+void UPIDControllerBPLibrary::ResetPID(FPIDCore& PIDCoreRef)
 {
-	PIDRef.Reset();
+	PIDCoreRef.Reset();
 }
--- Source/PIDController/Public/PIDControllerBPLibrary.h
+++ Source/PIDController/Public/PIDControllerBPLibrary.h
@@ -15,14 +15,14 @@ class PIDCONTROLLER_API UPIDControllerBPLibrary : public UBlueprintFunctionLibra
 
 public:
 	UFUNCTION(BlueprintCallable, Category = "PIDController")
-	static void InitPID(UPARAM(ref) FPIDCore& PIDRef, const float InKp = 0.0f, const float InKi = 0.0f, const float InKd = 0.0f);
+	static void InitPID(UPARAM(ref) FPIDCore& PIDCoreRef, const float InKp = 0.0f, const float InKi = 0.0f, const float InKd = 0.0f);
 
 	UFUNCTION(BlueprintCallable, Category = "PIDController", meta = (ReturnDisplayName = "SignalValue"))
-	static float TickPID(UPARAM(ref) FPIDCore& PIDRef, const float Difference, const float DeltaTime);
+	static float TickPID(UPARAM(ref) FPIDCore& PIDCoreRef, const float Difference, const float DeltaTime);
 
 	UFUNCTION(BlueprintPure, Category = "PIDController", meta = (ReturnDisplayName = "SignalValue"))
-	static float GetSignalPID(UPARAM(ref) const FPIDCore& PIDRef);
+	static float GetSignalPID(UPARAM(ref) const FPIDCore& PIDCoreRef);
 
 	UFUNCTION(BlueprintCallable, Category = "PIDController")
-	static void ResetPID(UPARAM(ref) FPIDCore& PIDRef);
+	static void ResetPID(UPARAM(ref) FPIDCore& PIDCoreRef);
 };
